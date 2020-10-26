2476d9ac2e9b071d795ba6f117a4c6679c16dd37
Victor García <vgdobon91@gmail.com>
2020-10-26 19:19:47 +0100

    Recoger numeros de un input

--- 2-EstructuraDatos/media_suma_listas.py
+++ 2-EstructuraDatos/media_suma_listas.py
@@ -1,14 +1,23 @@
 import statistics as stats
 
-lista_numeros=[2, 4, 6, 7, 8, 10]
+lista=input("Lista de numeros:")
+lista=lista.split()
+print(lista)
+
+lista_numericos=[]
+for i in lista:
+    lista_numericos.append(int(i))
+print(lista_numericos)
+
+
 
 suma=0
-for i in lista_numeros:
+for i in lista_numericos:
     suma+=i
 
 print("Suma:",suma)
-print("Media:",suma/len(lista_numeros))
+print("Media:",suma/len(lista_numericos))
 
 
-print("Suma con metodos:",sum(lista_numeros))
-print("Media con metodos",stats.mean(lista_numeros))
+print("Suma con metodos:",sum(lista_numericos))
+print("Media con metodos",round(stats.mean(lista_numericos)))
