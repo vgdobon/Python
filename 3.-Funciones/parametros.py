eff729c0bb209414322c4f40190c24cb488d9914
Victor García <vgdobon91@gmail.com>
2020-11-03 19:48:07 +0100

    Funciones

--- 3.-Funciones/parametros.py
+++ 3.-Funciones/parametros.py
@@ -3,7 +3,7 @@ def irpf(base, porcentaje=12.5,prorrateado=False):
         irpf (sueldo,porcentaje, prorrateado=boolean)
     """
     if prorrateado:
-        cantidad_prorateada = base/6s
+        cantidad_prorateada = base/6
     if type(base)==float and type(porcentaje)==float:
         return (base/100) * porcentaje
     else:
