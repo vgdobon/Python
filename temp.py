d8a3ab00bc50b6ac06af4b0c945a7ca95f58a5fe
EpicDeveloper <vgdobon91@gmail.com>
2020-11-09 17:22:52 +0100

    Cambios

--- temp.py
+++ temp.py
@@ -1,4 +1,14 @@
 print(list(map(sum, zip([1, 2, 3], [4, 5, 6]))))
 
-lista = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+lista = [1, 2, 3, 4, 5, 6, 7, 8,8, 9]
 print(lista[:-1])
+print(set(lista))
+
+
+lista1 = []
+
+for i in range(10):
+    lista1.append(i)
+
+set_lista=set(lista1)
+print(set_lista)
