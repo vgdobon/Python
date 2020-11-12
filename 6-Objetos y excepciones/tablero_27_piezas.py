f0c88610ea1a3e2e32fa8f3e31bcc13140b53285
EpicDeveloper <vgdobon91@gmail.com>
2020-11-12 23:05:56 +0100

    Tablero

--- 6-Objetos y excepciones/tablero_27_piezas.py
+++ 6-Objetos y excepciones/tablero_27_piezas.py
@@ -1,29 +1,82 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import random
 class Tablero:
 
-    # def rellenar_tablero_inicial(self, abecedario):
-    #         letra = iter(abecedario)
-    #         tablero_inicial= [ ["","","","","","",""],["","","","","","",""],["","","","","","",""],["","","","","","",""] ]
-    #         for i in range(4):
-    #             for j in range(7):
-    #                 tablero_inicial[i][j]=letra.__next__()
+    def __init__(self):
+        self.tablero= self.rellenar_tablero_inicial()
+    
+    
+    def cadena_aleatoria(self):
+            cadena = ["A","B","C","D","E","F","G","H","I","J","K","L"," ","M","N","Ñ","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+            i=0
+
+            while i in range(len(cadena)):
+                numero_aleatorio = random.randrange(28)
+                aux=cadena[i]
+                cadena[i]=cadena[numero_aleatorio]
+                cadena[numero_aleatorio]=aux
+                i+=1
+            
+            return cadena
+
+    def rellenar_tablero_inicial(self):
+            abecedario = self.cadena_aleatoria()
+            letra = iter(abecedario)
+            tablero_inicial= [ ["","","","","","",""],["","","","","","",""],["","","","","","",""],["","","","","","",""] ]
+            for i in range(4):
+                for j in range(7):
+                    tablero_inicial[i][j]=letra.__next__()
                 
-    #         return tablero_inicial
+            return tablero_inicial
+
+    
 
 
-
-    def __init__(self,tablero_inicial):
-        # self.tablero_inicial= self.rellenar_tablero_inicial(tablero_inicial)
-        self.tablero_inicial= tablero_inicial
+        
     
     def dibujar_estado(self):
-        print(self.tablero_inicial)
+        for i in self.tablero:
+            print(i,"\n")
 
-    
-t=[["a", "h", "ñ", "u", "b", "i", "o"], ["v", "c", "j", "p", "w", "d", "k"], ["u", "x", "e", " ", "l", "r", "f"], ["m", "s", "y", "g", "n", "t", "z"]]
+    def mover(self,letra,direccion):
+        posicion_letra = list(((x,  y) for x,  row in enumerate(self.tablero)
+                        for y,  elemento in enumerate(row)  if elemento == letra))
+        if direccion=="N":
+            
+            print(busqueda)
+            # if (posicion[0]>3 or posicion[0]<0)  or (posicion[1]>6 or posicion[1]<0):
+            pass
+        elif direccion=="S":
+            pass
+        elif direccion=="E":
+            pass
+        elif direccion=="0":
+            pass
+        else:
+            return "No has elegido una direccion correcta (N S E 0)"
 
-tablero = Tablero(t)
+    def comprobacion(self,tablero_comprobacion):
+        tablero_acertado = [ ["A","B","C","D","E","F","G"],["H","I","J","K","L","M","N"],["Ñ","O","P","Q","R","S","T"],["U","V","W","X","Y","Z",""] ]
+        i=0
+        while i < len(tablero_comprobacion):
+            j=0
+            while j < len(tablero_comprobacion[0]):
+                if not tablero_comprobacion[i][j] == tablero_acertado[i][j]:
+                    return False
+        return True
 
-tablero.dibujar_estado()
+
+juego_tablero = Tablero()
+while not juego_tablero.comprobacion(juego_tablero.tablero):
+    print("Mueve ficha")
+    letra= input("Letra a mover")
+    direccion= input("En que dirreccion (N,S,E,O)")
+    juego_tablero.mover(letra,direccion)
+    juego_tablero.dibujar_estado()
+    print(juego_tablero.dibujar_estado())
+
+
+print("HAS GANADO")
+
--- temp.py
+++ temp.py
@@ -17,18 +17,34 @@
 #     """Calcula y devuelve el conjunto potencia del 
 #        conjunto c.
 #        Devuelve todas las combinaciones en una lista
-#     """
-#     if len(c) == 0:
-#         return [[]]
-#     r = potencia(c[:-1])
-#     return r + [s + [c[-1]] for s in r]
+# #     """
+# #     if len(c) == 0:
+# #         return [[]]
+# #     r = potencia(c[:-1])
+# #     return r + [s + [c[-1]] for s in r]
+# import random
+
+# def cadena_aleatoria():
+#     cadena = "ABCDEFGHIJKLMN ÑOPQRSTUVWXYZ"
+#     print(cadena)
+#     print(len(cadena))
+#     i=0
+#     while i < len(cadena):
+#         numero_aleatorio = random.randrange(28)
+#         aux = cadena[i]
+#         print()
+#         cadena[i].cadena[numero_aleatorio] )
+#         cadena[numero_aleatorio].replace( aux )
+#         i+=1
+        
+
+#     return cadena
+
+# print(cadena_aleatoria())
+
+tablero_acertado = [ ["A","B","C","D","E","F","G"],["H","I","J","K","L","M","N"],["Ñ","O","P","Q","R","S","T"],["U","V","W","X","Y","Z",""] ]
 
 
+print(tablero_acertado.index("A"))    
 
-<<<<<<< HEAD
-
-=======
-tablero_inicial[4][7]
-print(len(tablero_inicial[0]))
-print(len(tablero_inicial))
->>>>>>> 55889c5cdee872f41508f8f8290f6b7c0050c046
+# np.where(tablero_acertado =="A")
