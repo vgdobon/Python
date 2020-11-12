2c717f837797dc29f2156cb7ea2dbf82ff465e01
Victor García <vgdobon91@gmail.com>
2020-11-12 20:23:55 +0100

    fhafd

--- elegir_compañero_aleatorio.py
+++ elegir_compañero_aleatorio.py
@@ -6,4 +6,4 @@ for i in range(100):
     "Victor GI","Victor GD","Ignacio Gil","Tania Gonzalez","Belen Lopez","Vanessa Mayayo","Ignacio Morer",
     "Pablo Nieto","Antonio Olona","Alvaro Sanz","Laura Soria"))
 
-print("Ha sido elegido :",xRandom) 
+print("Ha sido elegido :",xRandom)  
