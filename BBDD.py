ae5b80b0c6aa53df63021cf50925858c9700a001
EpicDeveloper <vgdobon91@gmail.com>
2020-11-23 22:03:04 +0100

    venv BBDD

---
+++ BBDD.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import pandas
+import sqlalchemy
+
+engine = sqlalchemy.create_engine('postgresql://censo:620312786@192.168.56.2:5432/geografia')
+
